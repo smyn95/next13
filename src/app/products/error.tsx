a0d7283164d4dde9ca442a5e8a1c6d08094a3985
smyn95 <tlsalduszz@gmail.com>
2023-07-15 15:04:23 +0900

    loading, error, image tag

---
+++ src/app/products/error.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+
+type Props = { error: Error & { digest?: string }; reset: () => void };
+
+export default function Error({ error, reset }: Props) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div>
+            <h2>{error.message}</h2>
+            <button onClick={() => reset()}>Try again</button>
+        </div>
+    );
+}
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,19 @@
 import MeowArticle from '@/components/MeowArticle';
 import { getProducts } from '../../service/products';
+import clothesImage from '../../../public/images/clothes.jpg';
 import Link from 'next/link';
+import Image from 'next/image';
 
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
+    // throw new Error();
     const products = await getProducts();
 
     return (
         <>
             <h1>제품 소개 페이지</h1>
+            <Image src={clothesImage} alt="clothes" />
             <ul>
                 {products.map(({ id, name }, index) => (
                     <li key={index}>
